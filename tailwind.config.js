cd919967f23c8b63d6d5dfa1f6c3bbd8296642d8
Nikolay M. <vindetto4004@gmail.com>
2025-01-24 08:21:51 +0300

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/*.html'],  // Следим за всеми папками и файлами .html
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
