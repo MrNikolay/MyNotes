6e1591fdcf9c496ffd30dfa6019cc5d68fbc0490
Nikolay M. <vindetto4004@gmail.com>
2025-01-27 07:57:02 +0300

    Доделал хэдер с анимацией

--- src/js/main.js
+++ src/js/main.js
@@ -1,14 +1,30 @@
-const btn = document.querySelector('.btn');
+// Проверка анимаций
+// const closingHeaderAnimation = document.getElementById('closing-header-animation');
+const littleHeader = document.querySelector('.little-header');
+const bigHeader = document.getElementById('big-header');
 
-let isMenuUpen = false;
+// closingHeaderAnimation.id = '';
+littleHeader.hidden = true;
 
-btn.addEventListener('click', function() {
-    isMenuUpen = !isMenuUpen;
-    btn.className = `btn ${isMenuUpen ? 'active' : 'not-active'}`
-});
+let isMenuOpen = true;
 
-// var btn = $('.btn');
+function checkScroll() {
+    const y = window.scrollY;
+    if (isMenuOpen && y > 100) {
+        // Закрываем хэдер
+        bigHeader.style.opacity = '0';
+        isMenuOpen = false;
+        littleHeader.hidden = false;
+        littleHeader.classList.remove('close');
+        littleHeader.classList.add('open');
+        // closingHeaderAnimation.id = 'closing-header-animation';
+    } else if (!isMenuOpen && y < 100) {
+        bigHeader.style.opacity = '1';
+        isMenuOpen = true;
+        // closingHeaderAnimation.id = '';
+        littleHeader.classList.remove('open');
+        littleHeader.classList.add('close');
+    }
+}
 
-// btn.on('click', function() {
-//   $(this).toggleClass('active not-active');
-// });
+document.addEventListener('scroll', checkScroll);
