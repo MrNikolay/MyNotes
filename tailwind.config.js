c3e3dcd39c90f3217328524e34617c5477991e48
Nikolay M. <vindetto4004@gmail.com>
2025-01-29 06:28:50 +0300

    base react project setup

--- tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ['./src/*.html'],  // Следим за всеми папками и файлами .html
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
